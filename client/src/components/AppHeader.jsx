4b51bfb2ced718bf5c9220e6b57384adb8cd4c17
Castle <danaremut@email.com>
2026-03-22 06:10:55 +0000

    updated readme

--- client/src/components/AppHeader.jsx
+++ client/src/components/AppHeader.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const AppHeader = () => {
     return (
         <div className='header sticky'>
-            <h3 className=''>dts-developer-challenge</h3>
+            <h3 className=''>HMCTS Task Management System</h3>
             <p>Welcome to your dev test!</p>
         </div>
     )
